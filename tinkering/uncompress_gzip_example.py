743dc8e2984f565cb184ad3d355b69aceb431c40
Cam Mackintosh <cmackint@akamai.com>
2023-02-09 11:38:55 -0800

    [DDO-6] Add example script to uncompress GZip file
    Use the gzip module to uncompress a GZip archive.
    
    I validated this against an example Edge DNS log GZip.

---
+++ tinkering/uncompress_gzip_example.py
@@ -0,0 +1,23 @@
+import argparse
+import gzip
+import os
+import shutil
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog = 'uncompress_gzip_example',
+        description = 'Uncompress GZip file')
+    parser.add_argument('gzip_file_path', help="GZip file to uncompress")
+    args = parser.parse_args()
+
+    path, extension = os.path.splitext(args.gzip_file_path)
+    new_path: str = path + ".txt"
+
+    print("Uncompressing file " + args.gzip_file_path + " into " + new_path)
+
+    with gzip.open(args.gzip_file_path, 'rb') as compressed_file:
+        with open(new_path, 'wb') as uncompressed_file:
+            shutil.copyfileobj(compressed_file, uncompressed_file)
+
+    print("Finished uncompressing file")
+
