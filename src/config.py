8ca54fe89607cfe1e01f021d5da454b05d1bb398
Cam Mackintosh <cmackint@akamai.com>
2023-02-09 10:50:16 -0800

    [DDO-6] Add NetStorage API wrapper and configuration
    NetStorage has a usage API and a configuration API. The usage API is for
    downloading/uploading/listing/etc files. It has its own authorization
    scheme. The configuration API is for managing NetStorage configuration. It
    uses the OPEN authorization scheme.
    
    Akamai provides a Python NetStorage API wrapper. Use this. I've yet to
    validate this wrapper. The authorization version has been updated to the
    recommended, so I would expect there to be no issues. This wrapper
    provides some niceties like chunk-downloading files.
    
    NetStorage authorization headers:
    - https://techdocs.akamai.com/netstorage-usage/reference/example-authentication-headers
    
    NetStorage API Python wrapper:
    - https://github.com/akamai/NetStorageKit-Python

--- src/config.py
+++ src/config.py
@@ -2,14 +2,29 @@ import yaml
 import logging
 
 class Config:
+    # NetStorage has a usage API and a configuration API. 
+    # The usage API uses it's own authorization. These keys are managed by NetStorage
+    # The configuration API uses the normal OPEN authorization. These keys are managed by Identity + Access Management
+    # The NetStorage configuration API isn't likely needed
     KEY_OPEN = "akamai_open_token"
     KEY_OPEN_CLIENT_SECRET = "client_secret"
     KEY_OPEN_HOST = "host"
     KEY_OPEN_ACCESS_TOKEN = "access_token"
     KEY_OPEN_CLIENT_TOKEN = "client_token"
 
+    KEY_NS = "netstorage"
+    KEY_NS_HOST = "host"
+    KEY_NS_ACCOUNT = "upload_account"
+    KEY_NS_CP_CODE = "cp_code"
+    KEY_NS_KEY = "key"
+    KEY_NS_SSL = "use_ssl"
+
     KEY_SPLUNK = "splunk"
-    KEY_SPLUNK_ADDRESS = "address"
+    KEY_SPLUNK_HEC = "hec"
+    KEY_SPLUNK_HEC_HOST = "host"
+    KEY_SPLUNK_HEC_PORT = "port"
+    KEY_SPLUNK_HEC_TOKEN = "port"
+    KEY_SPLUNK_HEC_SSL = "use_ssl"
 
     def __init__(self, yaml_stream):
         # TODO: Consider migrating this to a static factory. 
@@ -24,6 +39,19 @@ class Config:
             self.open_host = open_config[Config.KEY_OPEN_HOST]
             self.open_access_token = open_config[Config.KEY_OPEN_ACCESS_TOKEN]
             self.open_client_token = open_config[Config.KEY_OPEN_CLIENT_TOKEN]
+
+            ns_config = config[Config.KEY_NS]
+            self.netstorage_host = ns_config[Config.KEY_NS_HOST]
+            self.netstorage_upload_account = ns_config[Config.KEY_NS_ACCOUNT]
+            self.netstorage_cp_code = ns_config[Config.KEY_NS_CP_CODE]
+            self.netstorage_key = ns_config[Config.KEY_NS_CP_CODE]
+            self.netstorage_use_ssl = ns_config[Config.KEY_NS_SSL]
+
+            hec_config = config[Config.KEY_SPLUNK][Config.KEY_SPLUNK_HEC]
+            self.hec_host = hec_config[Config.KEY_SPLUNK_HEC_HOST]
+            self.hec_port = hec_config[Config.KEY_SPLUNK_HEC_PORT]
+            self.hec_token = hec_config[Config.KEY_SPLUNK_HEC_TOKEN]
+            self.hec_use_ssl = hec_config[Config.KEY_SPLUNK_HEC_SSL]
         except KeyError as keyError:
             logging.error("Configuration file missing key %s", keyError.args[0])
             # TODO: Add termination handler to clean-up
--- src/connector.py
+++ src/connector.py
@@ -2,22 +2,41 @@ import logging
 import requests
 from urllib.parse import urljoin
 from akamai.edgegrid import EdgeGridAuth
+from akamai.netstorage import Netstorage, NetstorageError
+
+from config import Config
 
 class Connector:
-    def __init__(self, config):
+    def __init__(self, config: Config):
         self.config = config
 
         # Configure Akamai OPEN authorization using provided token
+        # TODO: NetStorage doesn't use OPEN. Remove this
         self.open_session = requests.Session()
         self.open_session.auth = EdgeGridAuth(
             client_token=config.open_client_token,
             client_secret=config.open_client_secret,
             access_token=config.open_access_token
         )
+
+        self.netstorage = Netstorage(
+            hostname=self.config.netstorage_host,
+            keyname=self.config.netstorage_upload_account,
+            key=self.config.netstorage_key,
+            ssl=self.config.netstorage_use_ssl
+        )
     
     def run(self):
         # Query Akamai API
         logging.info('Fetching available Akamai Edge locations')
-        result = self.open_session.get(urljoin('https://' + self.config.open_host, '/edge-diagnostics/v1/edge-locations'))
-        logging.info('Result code [%d]', result.status_code)
-        logging.info('Result [%s]', result.json())
+        response = self.open_session.get(urljoin('https://' + self.config.open_host, '/edge-diagnostics/v1/edge-locations'))
+        logging.info('Response code [%d]', response.status_code)
+        logging.info('Response [%s]', response.json())
+
+        # Get file from NetStorage
+        logging.info('Fetching available files from Akamai NetStorage')
+        (ok, response) = self.netstorage.list('/{0}/'.format(self.config.netstorage_cp_code))
+        logging.info('Response ok [%b]', ok) 
+        logging.info('Response [%s]', response.text if response != None else None)
+
+
